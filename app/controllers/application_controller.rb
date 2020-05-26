6fbac186046ea532dc4eb3ae38421b0805071f52
osha7 <ogroetz@gmail.com>
2020-05-26 14:03:28 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,52 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  #index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  #new
+  get '/articles/new' do
+    erb :new
+  end
+
+  #show
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #create
+  post '/articles' do
+    article = Article.create(params)
+    redirect to "/articles/#{article.id}"
+  end
+
+  #edit
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #update
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:edit_article])
+    @article.save
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #delete
+  delete '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    
+    redirect to '/articles'
+  end	
+
+
 end
